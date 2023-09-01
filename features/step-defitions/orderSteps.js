ea11f3f178d29ab53e5660a0a5debb28c117045b
Gita Meditha Nirwanduna <gitamedithanirwanduna@ip-192-168-1-4.ap-southeast-1.compute.internal>
2023-09-01 17:38:45 +0700

    commit -m

--- features/step-defitions/orderSteps.js
+++ features/step-defitions/orderSteps.js
@@ -1,16 +1,11 @@
+const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
-const api = require('../api/pages');
-const orderData = require('../data/orderData');
-const jsonMatcher = require('../json-matcher');
 
-var {defineSupportCode} = require('@cucumber/cucumber');
-
-defineSupportCode(function({Given, When, Then}) {
-
+const api = require('../../api/pages');
+const orderData = require('../../data/orderData');
 
 Given('a sample order', function () {
-  // Prepare or load sample order data
-  // For simplicity, let's assume orderData contains sample order
+
 });
 
 When('I process the order', async function () {
@@ -23,6 +18,4 @@ Then('the order status should be updated', function () {
 
 Then('the last updated timestamp should be updated', function () {
   expect(processedOrder.last_updated_timestamp).toBeGreaterThan(last_updated_timestamp);
-});
-
 });
